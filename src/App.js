30356a9825fb460951a2af29f5488693184d1e18
Gracelaura <gracenjuguna05@gmail.com>
2023-02-06 13:48:26 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Cart from './Components/Cart.js'
+import Login from './Components/Login.js'
 
 function App() {
   const [heading , setHeading ] = useState(""); 
@@ -21,6 +24,14 @@ function App() {
   return (
    <div className='font-main text-4xl p-5  bg-modal-bg text-heading min-h-screen align-middle items-center text-center'>
         { heading === "" ? <h1 className='animate-pulse border-b-4 border-btn-bg'> Sith Shop</h1> : <h1 className='animate-pulse border-b-4 border-btn-bg'>{heading}</h1> }
+        <Switch>
+          <Route exact path='/cart'>
+            <Cart />
+          </Route>
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+        </Switch>
    </div>
   );
 }
