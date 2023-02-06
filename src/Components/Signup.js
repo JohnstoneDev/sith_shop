4a0ce61344ec3b653689ba974e2f7d88a49403ff
Duncun Kiprop <dankiproh@gmail.com>
2023-02-06 13:47:29 +0300

    sign up page

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,1 +1,29 @@
 // Signup Form 
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className="signup_container">
+        <div className='signup_for'> 
+        <h3>SignUP Form Validation </h3>
+        <div className='form'>
+                <form>
+                    <label> Username:</label>
+                <input type= 'username' placeholder='Enter Username'/>
+
+                <label>Email:</label>
+                <input type='email' placeholder= 'Enter Email'/>
+
+                <label>Password:</label>
+                <input type = 'password' placeholder='Enter Password'/>
+
+                <label>Confirm Password:</label>
+                <input type='password' placeholder='Enter Confirm Password'/>
+            </form>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Signup
